b458945bdb644ce256b4ab88c44c5e948fbf78d5
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-13 17:15:27 +0300

    Update main.c
    am facut posibila introducerea celor 3fisiere din linia de comanda + verificari de erori

--- main.c
+++ main.c
@@ -1,25 +1,67 @@
 #include "tasks.h"
 
-void WriteTeams(TeamList *list)
+void errorInput(int argc, char *argv[])
 {
-    FILE *f = fopen("out/r.out", "wt");
+    if(argc != 4)
+    {
+        printf("datele de intrare trebuie sa fie la numar de trei fisiere: primele doua de intrare si ultimul de iesire\n");
+        exit(1);
+    }
+    if(!strstr(argv[1], "c.in"))
+    {
+        printf("primul fisier trebuie sa fie c.in\n");
+        exit(1);
+    }
+    if(!strstr(argv[2], "d.in"))
+    {
+        printf("al doilea fisier trebuie sa fie d.in\n");
+        exit(1);
+    }
+    if(!strstr(argv[3], ".out"))
+    {
+        printf("al treilea fisier trebuie sa fie r.out\n");
+        exit(1);
+    }
+}
+
+int* taskInput(char *fisier)
+{
+    FILE *f = fopen(fisier, "rt");
     if(!f)
     {
-        printf("fisierul r.out nu s-a putut deschide");
+        printf("nu s-a putut deschide fisierul c.in\n");
         exit(1);
     }
 
-    for(register TeamList* p = list; p; p = p->next)
-        fprintf(f, "%s\n", p->echipa->nume_echipa);
-        
+    int *v = (int*)malloc(sizeof(int) * 5);
+    if(!v)
+    {
+        printf("variabilei v nu i s-a putut aloca memorie\n");
+        exit(1);
+    }
+
+    for(register int  i = 0; i < 5; i++)
+        fscanf(f, "%d", &v[i]);
     fclose(f);
+
+    if(!v[0])
+    {
+        printf("nu exista date de intrare\n");
+        free(v);
+        exit(1);
+    }
+
+    return v;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    TeamList *lista = Input();
-    WriteTeams(lista);
+    errorInput(argc, argv);
+    int *task = taskInput(argv[1]);
 
-    delTeamList(&lista);
+    TeamList *lista_echipe = NULL;
+    task1(&lista_echipe, argv);
+
+    delTeamList(&lista_echipe);
     return 0;
 }
