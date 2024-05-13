5344409d5ed9bf8166301d67843ea85849fd15e2
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-13 17:17:58 +0300

    Update tasks.h
    biblioteca pentru task-uri

--- tasks.h
+++ tasks.h
@@ -1,37 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "libs.h"
 
-char* citireSir(FILE*, int);
-
-typedef struct{
-    char *nume, *prenume;
-    int punctaj;
-} Player;
-
-typedef struct a{
-    Player *concurent;
-    struct a *next; 
-} PlayerList;
-
-typedef struct{
-    char *nume_echipa;
-    int numar_concurenti;
-    PlayerList *lista_concurenti;
-} Team;
-
-typedef struct b{
-    Team *echipa;
-    struct b *next;
-} TeamList;
-
-Player* createPlayer();
-PlayerList* createPlayerList();
-Team* createTeam();
-TeamList* createTeamList();
-void addPlayer(PlayerList**, Player*);
-void addTeam(TeamList**, Team*);
-void delPlayerTeam(PlayerList**);
-void delTeamList(TeamList**);
-
-TeamList* Input();
+void task1(TeamList**, char *[]);
+// void task2
+// void task3
+// void task4
