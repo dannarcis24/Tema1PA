291624749e615f03334f4ff5bd84d0e67047b257
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-13 17:20:26 +0300

    noua librarie pentru functiile principale

---
+++ libs.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readString(FILE*, int);
+
+typedef struct{
+    char *nume, *prenume;
+    int punctaj;
+} Player;
+
+typedef struct a{
+    Player *concurent;
+    struct a *next; 
+} PlayerList;
+
+typedef struct{
+    char *nume_echipa;
+    int numar_concurenti;
+    PlayerList *lista_concurenti;
+} Team;
+
+typedef struct b{
+    Team *echipa;
+    struct b *next;
+} TeamList;
+
+Player* createPlayer();
+PlayerList* createPlayerList();
+Team* createTeam();
+TeamList* createTeamList();
+void addPlayer(PlayerList**, Player*);
+void addTeam(TeamList**, Team*);
+void delPlayerTeam(PlayerList**);
+void delTeamList(TeamList**);
+
+TeamList* Input(char*);
+void writeTeams(TeamList*, char*);
