f92ba20596c68434d10bf27edbcaf9f1203ff218
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-13 20:44:11 +0300

    Am terminat task 2

--- taskcodes.c
+++ taskcodes.c
@@ -1,10 +1,15 @@
 #include "tasks.h"
-void task1(TeamList **list, char *argv[])
+void task1(TeamList **list, int *numar, char *argv[])
 {
-    *list = Input(argv[2]);
+    *list = Input(argv[2], numar);
     writeTeams(*list, argv[3]);
 }
 
-// void task2()
+void task2(TeamList **list, int *numar, char *argv[])
+{
+    delTeams(list, numar);
+    writeTeams(*list, argv[3]);
+}
 // void task3()
 // void task4()
+// void task5()
