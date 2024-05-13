dd0693eff4e8cc8fab1590f116e324534d780c11
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-13 20:43:00 +0300

    Update tasks.h
    am terminat task 2

--- tasks.h
+++ tasks.h
@@ -1,6 +1,8 @@
 #include "libs.h"
 
-void task1(TeamList**, char *[]);
-// void task2
+// APELUL DE TASK (1, 2, 3, 4, 5)
+void task1(TeamList**, int*, char*[]);
+void task2(TeamList**, int*, char*[]);
 // void task3
 // void task4
+// void task5
