465b9ba1f4428d8a4ab96fcb4fa4ca4035f21ef2
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-17 20:11:34 +0300

    task 3

--- taskcodes.c
+++ taskcodes.c
@@ -10,6 +10,10 @@ void task2(TeamList **list, int *numar, char *argv[])
     delTeams(list, numar);
     writeTeams(*list, argv[3]);
 }
-// void task3()
+
+void task3(TeamList *list, Node **list_final, int numar, char *argv[])
+{
+    *list_final = top8(list, numar, argv);
+}
 // void task4()
 // void task5()
