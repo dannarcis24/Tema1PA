0ba4c91ea8c66ce5ce88e33fab142b27072c4511
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-17 22:24:43 +0300

    task 4 + schimbare tip lista_finala in TeamList

--- taskcodes.c
+++ taskcodes.c
@@ -11,9 +11,23 @@ void task2(TeamList **list, int *numar, char *argv[])
     writeTeams(*list, argv[3]);
 }
 
-void task3(TeamList *list, Node **list_final, int numar, char *argv[])
+void task3(TeamList *list, TeamList **list_final, int numar, char *argv[])
 {
     *list_final = top8(list, numar, argv);
 }
-// void task4()
+
+void task4(TeamList* list, Tree **root, char *argv[])
+{
+    *root = createBinarySearchTree(list);
+    FILE *f = fopen(argv[3], "at");
+    if(!f)
+    {
+        printf("nu s-a putut deschide fisierul %s\n", argv[3]);
+        exit(1);
+    }
+
+    fprintf(f, "\nTOP 8 TEAMS:\n");
+    writeTree(*root, f);
+    fclose(f);
+}
 // void task5()
