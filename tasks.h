de1ee3b441189c612d3a381e0b0490d940de7ceb
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-17 20:10:50 +0300

    task 3

--- tasks.h
+++ tasks.h
@@ -3,6 +3,6 @@
 // APELUL DE TASK (1, 2, 3, 4, 5)
 void task1(TeamList**, int*, char*[]);
 void task2(TeamList**, int*, char*[]);
-// void task3
+void task3(TeamList*, Node**, int, char*[]);
 // void task4
 // void task5
