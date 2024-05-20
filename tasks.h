a5c86f5050b7dcf0ffafdcc3f488f1775687cbbb
dannarcis24 <162482012+dannarcis24@users.noreply.github.com>
2024-05-20 22:28:52 +0300

    adaugare comentari

--- tasks.h
+++ tasks.h
@@ -1,8 +1,8 @@
 #include "structs.h"
 
-// APELUL DE TASK (1, 2, 3, 4, 5)
-void task1(TeamList**, int*, char*[]);
-void task2(TeamList**, int*, char*[]);
-void task3(TeamList*, TeamList**, int, char*[]);
-void task4(TeamList*, Tree**, char*[]);
-void task5(Tree**, Tree*, char*[]);
+// APELUL PENTRU TASK-URI (1, 2, 3, 4, 5)
+void task1(TeamList**, int*, char*[]);              // efectuarea citirii echipelor
+void task2(TeamList**, int*, char*[]);              // numarul de echipe devine putere a lui 2, daca este cazul, se vor face stergeri ale echipelor cu punctaj minim
+void task3(TeamList*, TeamList**, int, char*[]);    // simularea meciurilor si salvarea ultimelor 8echipe
+void task4(TeamList*, Tree**, char*[]);             // ordonarea celor 8echipe intr-un BST
+void task5(Tree**, Tree*, char*[]);                 // crearea unui AVL, bazat pe BST
